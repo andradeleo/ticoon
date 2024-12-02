d18d52a8fbcd50ec6c0979afd729137376fd84d0
Leonardo <contato.leoo0@gmail.com>
2024-12-02 20:46:22 -0300

    style: organize imports

--- server/src/server/index.ts
+++ server/src/server/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import { makeSignUpController } from "../factories/makeSignUpController";
 import { makeSignInController } from "../factories/makeSignInController";
+import { makeSignUpController } from "../factories/makeSignUpController";
 import { routeAdapter } from "./adapters/routeAdapter";
 
 const app = express();
@@ -11,5 +11,5 @@ app.post("/sign-up", routeAdapter(makeSignUpController()));
 app.post("/sign-in", routeAdapter(makeSignInController()));
 
 app.listen(3001, () => {
-  console.log("🚀 Server started at http://localhost:3001");
+	console.log("🚀 Server started at http://localhost:3001");
 });
