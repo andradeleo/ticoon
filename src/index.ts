1fea4d755b0aa28818ce2d2137a6bc3236c1a14f
Leonardo <contato.leoo0@gmail.com>
2024-12-04 01:30:39 -0300

    style: format entrypoint file

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import express from "express";
 
 const app = express();
 
-app.get("/example", (req, res) => {
+app.get("/example", (_, res) => {
   res.status(200).json({ message: "success" });
-})
+});
 
 app.listen(3001, () => {
-  console.log("🚀 App running in http://localhost:3001")
-})
+  // biome-ignore lint/suspicious/noConsoleLog: <explanation>
+  console.log("🚀 App running in http://localhost:3001");
+});
