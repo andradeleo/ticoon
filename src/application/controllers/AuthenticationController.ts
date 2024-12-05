e1f00b256e1e47868222637d177095d2946df596
Leonardo <contato.leoo0@gmail.com>
2024-12-04 23:34:42 -0300

    refactor: move sign-up logic to AuthenticationController

---
+++ src/application/controllers/AuthenticationController.ts
@@ -0,0 +1,41 @@
+import { hash } from "bcryptjs";
+import { UserAlreadyExists } from "../errors/UserAlreadyExists";
+import { prismaClient } from "../libs/prisma";
+import { z } from "zod";
+import type { Request, Response } from "express";
+
+const schema = z.object({
+  name: z.string().min(3),
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+class AuthenticationController {
+  async signIn(req: Request, res: Response): Promise<void> {
+    const { name, email, password } = schema.parse(req.body);
+
+    const userAlreadyExists = await prismaClient.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (userAlreadyExists) {
+      throw new UserAlreadyExists();
+    }
+
+    const hashedPassword = await hash(password, 10);
+
+    await prismaClient.user.create({
+      data: {
+        email,
+        name,
+        password: hashedPassword,
+      },
+    });
+
+    res.status(204).json({ message: "sucess" });
+  }
+}
+
+export default new AuthenticationController();
--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,13 @@
-import express, {} from "express";
-import { z } from "zod";
-import { hash } from "bcryptjs";
-import { prismaClient } from "./application/libs/prisma";
 import "express-async-errors";
+import express, {} from "express";
 import { errorHandler } from "./application/middlewares/errorHandler";
-import { UserAlreadyExists } from "./application/errors/UserAlreadyExists";
+import AuthenticationController from "./application/controllers/AuthenticationController";
 
 const app = express();
 
 app.use(express.json());
 
-const schema = z.object({
-  name: z.string().min(3),
-  email: z.string().email(),
-  password: z.string().min(6),
-});
-
-app.post("/sign-up", async (req, res) => {
-  const { name, email, password } = schema.parse(req.body);
-
-  const userAlreadyExists = await prismaClient.user.findUnique({
-    where: {
-      email,
-    },
-  });
-
-  if (userAlreadyExists) {
-    throw new UserAlreadyExists();
-  }
-
-  const hashedPassword = await hash(password, 10);
-
-  await prismaClient.user.create({
-    data: {
-      email,
-      name,
-      password: hashedPassword,
-    },
-  });
-
-  res.status(204).json({ message: "sucess" });
-});
+app.post("/sign-up", AuthenticationController.signIn);
 
 app.use(errorHandler);
 
