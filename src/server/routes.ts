da6be7621d549748ecd0a39f73d30cf31feaffe1
Leonardo <contato.leoo0@gmail.com>
2024-12-08 12:36:54 -0300

    refactor: delete findById route

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -9,6 +9,5 @@ router.post("/sign-in", AuthenticationController.signIn);
 
 router.post("/quiz", QuizController.create);
 router.get("/quiz", QuizController.findAll);
-router.get("/quiz/:id", QuizController.findById);
 
 export default router;
