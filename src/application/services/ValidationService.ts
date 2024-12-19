316b7f94b910bcbf526da21622c054647aa8e130
Leonardo <contato.leoo0@gmail.com>
2024-12-18 22:07:21 -0300

    refactor: add a better types in validation service

--- src/application/services/ValidationService.ts
+++ src/application/services/ValidationService.ts
@@ -1,66 +1,82 @@
 import type { SubmittedQuestionType } from "src/schemas/quiz";
 
-export class ValidationService {
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  public correctAnswers: any;
+interface IAnswerCheck {
+  id: string;
+  isCorrect: boolean;
+}
 
+interface IQuestionCheck {
+  id: string;
+  answer: IAnswerCheck[] | string;
+  experience?: number | null | undefined;
+}
+
+interface ICorrectAnswers {
+  question_id: string;
+  answer_id: string;
+  experience?: number | null | undefined;
+}
+
+interface IAnswers {
+  user_answer: string;
+  quiz_answer: string;
+  isCorrect: boolean;
+  experience: number | null | undefined;
+}
+
+interface IUserAnswers {
+  answers: IAnswers[];
+  numberOfCorrectQuestions: number;
+}
+
+export class ValidationService {
   public validateQuestion(
     submittedQuestions: SubmittedQuestionType[],
-    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-    createdQuestions: any,
-    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  ): any {
-    this.correctAnswers = this.setCorrectAnswers(createdQuestions);
+    createdQuestions: IQuestionCheck[],
+  ): IUserAnswers {
+    const correctAnswers = this.setCorrectAnswers(createdQuestions);
+    const userSubmittedAnswers = this.setCorrectAnswers(submittedQuestions);
 
-    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-    const resp: any = [];
+    const relationCorrectQuestionsAnswers = correctAnswers.map(
+      (answer: ICorrectAnswers, index: number) => {
+        const answerId = answer.answer_id;
+        const response = userSubmittedAnswers[index];
 
-    const submitted = this.setCorrectAnswers(submittedQuestions);
-
-    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-    // biome-ignore lint/complexity/noForEach: <explanation>
-    this.correctAnswers.forEach((answer: any, index: number) => {
-      const answerId = answer.answer_id;
-
-      const response = submitted[index];
-
-      resp.push({
-        user_answer: response.answer_id,
-        quiz_answer: answerId,
-        isCorrect: response.answer_id === answerId,
-        experience: answer.experience,
-      });
-    });
+        return {
+          user_answer: response.answer_id,
+          quiz_answer: answerId,
+          isCorrect: response.answer_id === answerId,
+          experience: answer.experience,
+        };
+      },
+    );
 
     return {
-      answers: resp,
-      numberOfCorrectQuestions: resp.filter(
-        (res: { isCorrect: boolean }) => res.isCorrect === true,
+      answers: relationCorrectQuestionsAnswers,
+      numberOfCorrectQuestions: relationCorrectQuestionsAnswers.filter(
+        (res: IAnswers) => res.isCorrect === true,
       ).length,
     };
   }
 
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  public setCorrectAnswers(questions: any): {
-    question_id: string;
-    answer_id: string;
-  }[] {
-    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-    return questions.map((qst: any) => {
+  public setCorrectAnswers(questions: IQuestionCheck[]): ICorrectAnswers[] {
+    return questions.map((qst: IQuestionCheck) => {
       const questionId = qst.id;
 
       const answer = Array.isArray(qst.answer)
-        ? qst.answer.find(
-            (ans: { isCorrect: boolean }) => ans.isCorrect === true,
-          )
+        ? qst.answer.find((ans: IAnswerCheck) => ans.isCorrect === true)
         : {
             id: qst.answer,
           };
 
+      if (!answer) {
+        throw new Error("Correct answer not found!");
+      }
+
       return {
         question_id: questionId,
         answer_id: answer.id,
-        experience: qst.experience,
+        experience: qst?.experience,
       };
     });
   }
