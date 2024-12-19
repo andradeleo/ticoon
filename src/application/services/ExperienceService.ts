3598d5db10f20835a6499b9c3b9099b495b4c0c9
Leonardo <contato.leoo0@gmail.com>
2024-12-18 22:25:01 -0300

    refactor: add better types into experience service

--- src/application/services/ExperienceService.ts
+++ src/application/services/ExperienceService.ts
@@ -1,9 +1,17 @@
+interface IAnswer {
+  isCorrect: boolean;
+  experience: number | null | undefined;
+}
+
+interface IQuestion {
+  experience: number;
+}
+
 export class ExperienceService {
-  private experience: number | null = 0;
-  private readonly EXPERIENCE_MULTIPLY = 0.25;
-  private readonly LEVEL_STEP = 1000;
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  getTotalExperience(questions: any, experience: any): number | null {
+  getTotalExperience(
+    questions: IQuestion[],
+    experience: number | null,
+  ): number | null {
     const experienceFromQuestions = questions.reduce(
       (total: number, { experience }: { experience: number }) => {
         const xp = experience ?? 0;
@@ -12,29 +20,13 @@ export class ExperienceService {
       0,
     );
 
-    const total =
-      experienceFromQuestions > 0 ? experienceFromQuestions : experience;
-
-    this.experience = total;
-
-    return this.experience;
+    return experienceFromQuestions > 0 ? experienceFromQuestions : experience;
   }
 
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  sumExperienceFromCorrectAnswers(answers: any[]): number {
+  sumExperienceFromCorrectAnswers(answers: IAnswer[]): number {
     return answers.reduce((acc, current) => {
-      return current.isCorrect ? acc + current.experience : acc + 0;
+      const amountOfExperience = current.experience ?? 0;
+      return current.isCorrect ? acc + amountOfExperience : acc + 0;
     }, 0);
-  }
-
-  calculateUserExperienceGained(
-    experience: number,
-    user_level: number,
-  ): number {
-    return (experience * this.EXPERIENCE_MULTIPLY) / user_level;
-  }
-
-  tranformExperiencIntoLevel(experience: number): number {
-    return Math.floor(experience / this.LEVEL_STEP);
   }
 }
